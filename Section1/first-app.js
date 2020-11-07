b45dfea9fbf404208f2d11ca8e4e3ed8670c9618
alan <a.calderon07@hotmail.com>
2020-11-07 00:42:10 -0300

    Section1.

---
+++ Section1/first-app.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+console.log('Hello World!');
+fs.writeFileSync('Hello.txt', 'Hello World in txt');
---
+++ Section1/first-app.ts
@@ -0,0 +1,5 @@
+import * as fs from 'fs'
+
+console.log('Hello World!');
+
+fs.writeFileSync('Hello.txt', 'Hello World in txt');
